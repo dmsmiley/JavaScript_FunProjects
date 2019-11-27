e5dd313c0e28db3ede118baca45e2360f868564c
dmsmiley <davidmsmiley@gmail.com>
2019-11-27 10:44:43 -0500

    Create CaesarCipher.js

---
+++ CaesarCipher.js
@@ -0,0 +1,14 @@
+//Caesar Cipher that only works for uppercase letters
+function rot13(str) {
+  var input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var output = 'NOPQRSTUVWXYZABCDEFGHIJKLM';
+  //function for finding the currect index value of the chr
+  var index = x => input.indexOf(x);
+  //function for the index value and if it is greater than -1 it will output the corresponding output index chr for x
+  var translate = x => index(x) > -1 ? output[index(x)] : x;
+  //split the chr input, replace chr with the translate function and map into new array, then join the new chr in a new str
+  return str.split('').map(translate).join('');
+}
+// Change the inputs below to test
+a = rot13("SERR PBQR PNZC");
+console.log(a);
